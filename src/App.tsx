8a848e4f97c19146227939c8987fe9fa2c50dc43
Fuad2199 <fuad.beybutov099@gmail.com>
2025-06-24 19:32:04 +0400

    chore: initialize expense-tracker project with basic setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='w-full xl:h-screen flex items-center relative xl:p-7 py-0 px-2'>
+      <div className="h-full xl:w-[calc(100%-250px)] w-[calc(100%-80px)] bg-background xl:ml-17.5 xl:rounded-2xl flex xl:flex-row flex-col items-start border border-main-border/70"></div>
+    </div>
+  )
+}
+
+export default App
