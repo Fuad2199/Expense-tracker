f4c3ef588f74f9bc9dd67321d4134fa126677cf7
Fuad2199 <fuad.beybutov099@gmail.com>
2025-06-26 22:14:29 +0400

    style: removed dark background from root and fixed 'still' styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
-import './App.css'
-import Dashboard from './components/Dashboard'
-import Profile from './components/Profile'
-import Sidebar from './components/Sidebar'
+import "./App.css";
+import Dashboard from "./components/Dashboard";
+import Profile from "./components/Profile";
+import Sidebar from "./components/Sidebar";
 
 function App() {
-
   return (
-    <div className='2xl:w-full xl:h-screen flex items-center relative xl:p-7 py-0 px-2'>
-      <Sidebar/>
+    <div className="2xl:w-full xl:h-screen flex items-center relative xl:p-7 py-0 px-2">
+      <Sidebar />
       <div className="xl:w-280 h-full bg-background xl:ml-[200px] ml-[70px] xl:rounded-2xl flex xl:flex-row flex-col items-start border border-main-border/70">
-      <Dashboard/>
-      <Profile/>
+        <Dashboard />
+        <Profile />
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -14,7 +14,7 @@ const Dashboard: React.FC = () => {
   }, [transactions]);
 
   return (
-    <div className="xl:w-[85%] w-full flex flex-col xl:py-7 xl:pl-7 xl:pr-5 p-5">
+    <div className="2xl:w-[80%] xl:w-[65%] w-full flex flex-col xl:py-7 xl:pl-7 xl:pr-5 p-5">
       <div className="flex flex-col gap-1">
         <div className="text-3xl font-bold text-primary">Hey There Fuad!</div>
         <span className="text-sm text-primary">
